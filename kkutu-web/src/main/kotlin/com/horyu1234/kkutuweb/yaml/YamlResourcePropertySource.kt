5710e5c8a8b7bd32ddad440591c614e03342fbad
horyu1234 <horyu1234@naver.com>
2018-10-21 14:06:24 +0900

    [kkutu-web] 어플리케이션 시작 시 기본 파일 복사기능 제거, JPA 적용

--- kkutu-web/src/main/kotlin/com/horyu1234/kkutuweb/yaml/YamlResourcePropertySource.kt
+++
@@ -1,7 +0,0 @@
-package com.horyu1234.kkutuweb.yaml
-
-import org.springframework.core.env.PropertiesPropertySource
-import org.springframework.core.io.support.EncodedResource
-
-internal class YamlResourcePropertySource
-constructor(name: String, resource: EncodedResource) : PropertiesPropertySource(name, YamlPropertiesProcessor(resource.resource).createProperties())
